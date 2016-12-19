d38cc6dfde47acbbe40f1ee3e64bbc65e5a435b1
restot <92ilya.icom@gmail.com>
2016-12-19 08:36:58 -0800

    done work for now  Mon Dec 19 08:36:58 PST 2016

--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -67,41 +67,4 @@ class ReaderController < ApplicationController
         end
     end
 
-
-  def vendor1
- 	  @file = MyFile.find(params[:reader_id])
-
-	  @status = Status.find_by(model_id: @file.vendor_id.to_s)
-
-	  @status.update(status: "loading...", time_start: Time.now, time_end: nil)
-
-	  @xlsx = Roo::Spreadsheet.open(root_url + @file.attachment_url, extention: "xlsx")	# optional metods
-
-          model="Vendor#{@file.vendor_id.to_s}".constantize
-
-	  query = execute_statement("DELETE FROM vendor#{@file.vendor_id.to_s}s")
-
-	  query1 = execute_statement("delete from sqlite_sequence where name='#{model.to_s.downcase}s'")
-
-	 @xlsx.sheet(0).each do |row|
-                   @hash = Hash.new 
-	 	   i=0
-		 row.each do |cell|
-		   
-		   @hash["col_#{i.to_s}"] = cell
-		   i += 1
-
-		 end
-        	@ie = model.create(@hash)
-	        @ie.save
-
-	 end
-	  @status.update(status: "finish", time_end: Time.now, time_start: nil)
-	 	  respond_to do |f|
-		  f.html
-		  f.json {render json: "123",status: :ok}
-	  	end
-  end
-
-
 end
