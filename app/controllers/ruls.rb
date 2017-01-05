3396755f0f56b621020a30718b839dbe33ce6c12
92ilya.icom@gmail.com <92ilya.icom@gmail.com>
2017-01-05 17:36:52 +0000

    some code Thu Jan  5 17:36:52 UTC 2017

--- app/controllers/ruls.rb
+++ app/controllers/ruls.rb
@@ -35,8 +35,39 @@ class Ruls
     return  100 # INFO: code 100 means ruls is nil
     
     end
-    
   end
+
+    def self.price (ruls, flag = false)
+   
+    if ruls != nil then 
+      parsed_ruls = Ruls.json_parse(ruls)
+      splited_ruls = parsed_ruls["price"].split("|")
+      flag = splited_ruls[1]
+      splited_ruls_array = splited_ruls[0].split(",")
+      if flag.to_s == "true" then
+        return "Ruls: #{ splited_ruls_array.join(',')  } Flag TRUE"
+      else
+        ruls_array = Array.new()
+        t = splited_ruls_array.each do |e|
+          ruls_hash = Hash.new()
+          v = e.split(":")
+          ruls_hash[:operator] = v[0]
+          ruls_hash[:value] = v[1]
+          ruls_array << ruls_hash
+        end
+        print "##{__LINE__.to_s} " if DEBUG 
+        puts ruls_array if DEBUG 
+        puts "Ruls: #{ splited_ruls_array.join(',')  } Flag FALSE" if DEBUG
+        return ruls_array
+      end
+    else
+    return  100 # INFO: code 100 means ruls is nil
+    
+    end  
+    end
+    
+    
+ 
    def self.json_parse (json)
 
     hash = JSON.parse(json)
